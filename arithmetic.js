290e0898f898e97ce85049ca7fde54d1f333af4f
Zachary Invergo <zinvergo@gmail.com>
2021-04-12 23:34:55 -0500

    beginning of Zenva Javascript course, with intro, operators and beginning of conditional statements

---
+++ arithmetic.js
@@ -0,0 +1,30 @@
+// Addition
+let a = 1 + 1;
+let b = 10;
+let c = a + b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+// Subtraction
+let x = 10 - 5;
+console.log(x);
+let y = b - x;
+console.log(y);
+
+x = x - 1;
+
+// Multiplication
+let unitPrice = 2;
+let units = 10;
+let total = units * unitPrice;
+
+console.log(total);
+
+// division
+let n = 10;
+let result = n / 2;
+
+// Modulus (Remainder and/or can be used for T/F as a numerical Boolean)
+let r = 5 % 2;
+console.log(r);
