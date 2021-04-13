3ad9ea0e63dbe971c3b87c9ddd47fcd68dcde74f
Zachary Invergo <zinvergo@gmail.com>
2021-04-13 02:54:18 -0500

    tutorials on comparison and conditional operators

---
+++ comparison.js
@@ -0,0 +1,20 @@
+// '==' usage
+
+let item = "window";
+
+if (item == "engine") {
+  console.log("it's an engine!");
+} else if (item != "engine") {
+  console.log("that's not an engine!");
+}
+
+let score = 60;
+
+if (score >= 60) {
+  console.log("pass");
+} else {
+  console.log("fail");
+}
+
+let isEngine = item == "engine";
+console.log(isEngine);
---
+++ conditionals.js
@@ -0,0 +1,14 @@
+// let direction = "right";
+let direction = "left";
+// let direction = "backwards";
+// let direction = "up";
+
+if (direction == "left") {
+  console.log("red planet");
+} else if (direction == "right") {
+  console.log("purple planet");
+} else if (direction == "backwards") {
+  console.log("back home");
+} else {
+  console.log("crash");
+}
