a35201cde14e6df00330a50896963c613f3bc546
Zachary Invergo <zinvergo@gmail.com>
2021-04-14 00:08:51 -0500

    DOM API intro and conclusion of course

---
+++ domApi.js
@@ -0,0 +1,14 @@
+// 1. Select
+let sign = document.getElementById("sign");
+
+// 2. Modify
+console.log(sign.textContent);
+
+// modify text
+sign.textContent = "Welcome, travelers!";
+
+// // modify html
+// sign.innerHTML = '<p> Hello</p>'
+
+// 3. Change Style
+sign.style.color = "blue";
